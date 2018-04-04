d4c440405a0d96b2d085c3d63d6eedf2f1aad712
ShaimaaDerbaz <shaimaa.derbaz@gmail.com>
2018-04-04 20:28:45 +0200

    add invoice activity completed

---
+++ app/src/main/java/com/hadeya/tabonhandapp/activities/transaction/invoices/InvoiceItemsList.java
@@ -0,0 +1,13 @@
+package com.hadeya.tabonhandapp.activities.transaction.invoices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InvoiceItemsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity15_invoice_items_list);
+    }
+}
