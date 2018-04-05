dd0cda33dc6b93ce33a514e80e0a1951f9c9c06a
ShaimaaDerbaz <shaimaa.derbaz@gmail.com>
2018-04-05 15:35:26 +0200

    access to print invoice activity from save button

--- app/src/main/java/com/hadeya/tabonhandapp/activities/transaction/invoices/AddItemsInvoice.java
+++ app/src/main/java/com/hadeya/tabonhandapp/activities/transaction/invoices/AddItemsInvoice.java
@@ -1,9 +1,15 @@
 package com.hadeya.tabonhandapp.activities.transaction.invoices;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.hadeya.tabonhandapp.R;
+
+import butterknife.ButterKnife;
 
 public class AddItemsInvoice extends AppCompatActivity {
 
@@ -11,5 +17,18 @@ public class AddItemsInvoice extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity14_add_item_to_invoice_main);
+        ButterKnife.bind(this);
+        Button saveInvoice = (Button) findViewById(R.id.save);
+        saveInvoice.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick (View v){
+                Intent main = new Intent("PrintInvoice");
+                startActivity(main);
+            }
+        }
+
+        );
     }
 }
