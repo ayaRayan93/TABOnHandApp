b6d182f6e4600f58b8312556ebdd66e6f4d3b381
ShaimaaDerbaz <shaimaa.derbaz@gmail.com>
2018-04-05 14:53:57 +0200

    add print invoice activity ui

---
+++ app/src/main/java/com/hadeya/tabonhandapp/activities/transaction/invoices/PrintInvoice.java
@@ -0,0 +1,15 @@
+package com.hadeya.tabonhandapp.activities.transaction.invoices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hadeya.tabonhandapp.R;
+
+public class PrintInvoice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity16_print_invoice_main);
+    }
+}
